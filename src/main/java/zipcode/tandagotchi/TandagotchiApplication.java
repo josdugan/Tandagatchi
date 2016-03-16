2f94aa41e247d9e659d6dc80d813a9c496456f56
Shaun Gould <shaungould@gmail.com>
2016-03-16 16:09:13 -0400

    first commit on fork

---
+++ src/main/java/zipcode/tandagotchi/TandagotchiApplication.java
@@ -0,0 +1,12 @@
+package zipcode.tandagotchi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TandagotchiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TandagotchiApplication.class, args);
+	}
+}
---
+++ src/test/java/zipcode/tandagotchi/TandagotchiApplicationTests.java
@@ -0,0 +1,18 @@
+package zipcode.tandagotchi;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.test.context.web.WebAppConfiguration;
+import org.springframework.boot.test.SpringApplicationConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+
+@RunWith(SpringJUnit4ClassRunner.class)
+@SpringApplicationConfiguration(classes = TandagotchiApplication.class)
+@WebAppConfiguration
+public class TandagotchiApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
